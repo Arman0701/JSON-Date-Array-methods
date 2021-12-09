e173dd11d44425a1192514d1ce8d0c1608421629
Arman773 <tadevosyan889@gmail.com>
2021-12-09 23:58:05 +0400

    added task2 of reduce method

---
+++ Tasks/Reduce-method/2.Average-of-ages.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Write a function which calculates average age of users.
+
+function getAverageAge(arr){
+    if (!(arr instanceof Array)) return
+    return arr.map(elem => {return elem.age}).reduce((a,b) => {return a+b}) / 2;
+}
+
+const users = [
+    {
+        username: "Yuri Gagarin",
+        lang: "ru",
+        age: 56,
+    },
+    {
+        username: "Nil Armstrong",
+        lang: "ENG",
+        age: 54,
+    },
+];
+
+console.log(getAverageAge(users)); // 55
