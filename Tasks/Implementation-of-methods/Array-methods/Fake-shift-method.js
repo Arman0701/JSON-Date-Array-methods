e9385d51d99ac15d3a40735bb072baa38c772005
Arman773 <tadevosyan889@gmail.com>
2021-12-12 16:35:34 +0400

    feat: added implementation of pop method

---
+++ Tasks/Implementation-of-methods/Array-methods/Fake-shift-method.js
@@ -0,0 +1,11 @@
+'use strict'
+
+Array.prototype.fakeShift = function (){
+    let result = this[0];
+    this.length--;
+    return result;
+}
+
+let arr = [1,2,3,4,5];
+console.log(arr.fakeShift());
+console.log(arr.length);
