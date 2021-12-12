82bfa3bcd97a55b7de4f878f059a49941d7cc245
Arman773 <tadevosyan889@gmail.com>
2021-12-12 17:51:09 +0400

    feat: added implementation of join method

---
+++ Tasks/Implementation-of-methods/Array-methods/Fake-join-method.js
@@ -0,0 +1,19 @@
+'use strict'
+
+Array.prototype.fakeJoin = function(separator){
+    if (typeof separator !== 'string') return
+    let result = '';
+    for (let i=0; i<this.length; i++){
+        if (i === this.length-1){
+            result += this[i];
+            continue;
+        }
+        result += this[i] + separator;
+    }
+    return result;
+}
+
+console.log([1,2,3,4,5].fakeJoin(' '));
+console.log([1,2,3,4,5].fakeJoin(''));
+console.log([1,2,3,4,5].fakeJoin(' # '));
+console.log(typeof [1,2,3,4,5].fakeJoin(' '));
