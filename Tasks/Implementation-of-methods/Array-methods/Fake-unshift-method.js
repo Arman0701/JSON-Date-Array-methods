d9a65376121eec2f0e198e3fcee2d49097f3c14a
Arman773 <tadevosyan889@gmail.com>
2021-12-13 17:29:37 +0400

    fix: fixed wrong code

--- Tasks/Implementation-of-methods/Array-methods/Fake-unshift-method.js
+++ Tasks/Implementation-of-methods/Array-methods/Fake-unshift-method.js
@@ -1,11 +1,16 @@
 'use strict'
 
 Array.prototype.fakeUnshift = function(...args){
+    this.length += args.length;
     for (let i=0; i<this.length; i++){
-        args[args.length] = this[i];
-    }    
-    return args;
+        this[this.length-1-i] = this[this.length-1-args.length-i]
+    }
+    for (let i=0; i<args.length; i++){
+        this[i] = args[i];
+    }
+    return this;
 }
 
-console.log([1,2,3,4,5].fakeUnshift(-5,-4,-3,-2,-1,0));
+console.log([1,2,3,4,5,6,7,8,9].fakeUnshift(-5,-4,-3,-2,-1,0));
+console.log([5,6,7,8,9].fakeUnshift(-5,-4,-3,-2,-1,0,1,2,3,4));
 console.log([1,2,3,4,5].fakeUnshift(-5,'hello', true, null, undefined));
